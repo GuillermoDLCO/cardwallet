10c310f9abebc0252a7fc9a55bce88cd59193917
Guillermo De La Cruz <guillermo.dlco@outlook.com>
2019-10-08 10:23:47 -0500

    Initial commit

---
+++ lib/helpers/card_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardColor {
+  static const baseColors = <Color> [
+    Color.fromRGBO(61, 132, 223, 1.0), // Light blue
+    Color.fromRGBO(114, 71, 200, 1.0), // Purple
+    Color.fromRGBO(106, 132, 121, 1.0), // Green
+    Color.fromRGBO(229, 132, 223, 1.0), // Pink
+    Color.fromRGBO(96, 132, 223, 1.0), // Cyan
+    Color.fromRGBO(219, 132, 223, 1.0), // Orange
+    Color.fromRGBO(60, 132, 223, 1.0), // Black
+    Color.fromRGBO(222, 132, 223, 1.0), // Salmon
+    Color.fromRGBO(128, 132, 223, 1.0), // Light Cyan
+  ];
+}
---
+++ lib/ui/app.dart
@@ -0,0 +1,41 @@
+import 'package:cardwallet/ui/widgets/card_list.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          'Wallet',
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: CardList(),
+    );
+  }
+}
+
